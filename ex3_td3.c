7666d2f8be57dc4608b18f8ad7ca70f5ad549e4b
zung31 <danghoangdung2004@gmail.com>
2024-10-22 18:14:27 +0200

    Update ex3_td3.c

--- ex3_td3.c
+++ ex3_td3.c
@@ -1,27 +1,48 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
-#include <sys/wait.h>
 #include <signal.h>
-#include<string.h>
+#include <stdlib.h>
 
-void f1(int s){
+volatile sig_atomic_t counter = 0;
+pid_t child_pid; // Khai báo biến toàn cục để lưu trữ PID của tiến trình con
 
+void handle_ping(int sig) {
+    counter++;
+    printf("ping %d ", counter);
+    fflush(stdout); // Đảm bảo rằng giá trị được in ra ngay lập tức
+    kill(getppid(), SIGUSR1); // Gửi tín hiệu SIGUSR1 đến tiến trình cha (pong)
 }
 
-void f2(int s){
-
+void handle_pong(int sig) {
+    counter++;
+    printf("pong %d ", counter);
+    fflush(stdout);
+    kill(child_pid, SIGUSR2); // Gửi tín hiệu SIGUSR2 đến tiến trình con (ping)
 }
 
-int main(){
-    pid_t p;
-    p = fork();
-    if(p<0){
-        perror("Fil n'est pas crée");
+int main() {
+    pid_t pid;
+    pid = fork();
+    if (pid < 0) {
+        perror("fork");
         exit(1);
     }
-    if(p == 0){
-        
+    if (pid == 0) { // child (ping)
+        signal(SIGUSR2, handle_ping); 
+        for (;;) {
+            pause(); // Chờ tín hiệu SIGUSR2
+        }
+    } 
+    else { // pere (pong)
+        child_pid = pid; // PID de fil
+        signal(SIGUSR1, handle_pong); 
+        sleep(1); 
+        for (int i = 0; i < 10; i++) {
+            kill(child_pid, SIGUSR2); // envoyer SIGUSR2 à fil
+            pause(); // attendre SIGUSR1 de fil
+        }
+        kill(child_pid, SIGKILL); // terminer fil à la fin
     }
+
     return 0;
-} 
+}
